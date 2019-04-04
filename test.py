16ed2ebafef4e84bdc3352395b8605df0a76cb54
Erfan <net.crowmaster@gmail.com>
2019-04-04 23:46:39 +0430

    Initial Commit

--- test.py
+++ test.py
@@ -4,6 +4,8 @@
 # from keras import layers
 # from keras import Input
 from collections import defaultdict
+import numpy as np
+from sklearn import preprocessing
 
 
 def tuple_list_2_dict(da_list):
@@ -15,3 +17,12 @@ def tuple_list_2_dict(da_list):
 
 my_list = [(1, 'a'), (2, 'b'), (3, 'a'), (4, 'b')]
 print(tuple_list_2_dict(my_list))
+
+a = np.array([[1, 2, 3],
+              [1, 5, 6],
+              [1, 7, 9]])
+scaled_train_data = preprocessing.maxabs_scale(a[:-1, ], axis=0)
+print(scaled_train_data)
+print(scaled_train_data.mean(axis=0))
+print(a.mean(axis=0))
+print(a.std(axis=0))
